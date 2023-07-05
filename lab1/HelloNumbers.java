8a5cdc73eb86187d8a3331f642286f4a464a1a6c
Weipeng Sun <3595543050@qq.com>
2023-07-05 09:06:46 +0800

    第二次提交

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -2,8 +2,6 @@ public class HelloNumbers {
     public static void main(String[] args) {
         int x = 1;
         int total = 0;
-        System.out.println(5 + "10");
-        System.out.println(5 + 10);
         while (x <= 10) {
             System.out.print(total + " ");
             total = total + x;
